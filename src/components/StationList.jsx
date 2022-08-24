62c9d447fa51e97e8785b28c0c4b29745c5890ea
Raunit Kohli <rkohli@rocketsoftware.com>
2022-08-24 11:59:28 -0400

    commiting new station card changes

--- src/components/StationList.jsx
+++ src/components/StationList.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import StationCard from "./StationCard";
+//import { FlatList, Text, View } from 'react-native';
+
 
 const StationList = (props) => {
-    
+
+    /*const Item = ({ title }) => (
+      <View
+        style={{
+          backgroundColor: '#f9c2ff',
+          padding: 20,
+          marginVertical: 8,
+          marginHorizontal: 16,
+        }}>
+        <Text>{title}</Text>
+      </View>
+    ); */
+
     return (
         <div>
-        { 
+        {
             props.stationData.map(data => {
                 return (
-                    <StationCard data={data}/> 
-                )})   
+                    <StationCard data={data}/>
+            )})
         }
         </div>
     )
